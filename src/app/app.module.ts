aa4a5da13d0a825a5577d24d0468dc80659214c4
Mikael Simões <mikaelrsimoes19@gmail.com>
2021-05-05 13:57:58 -0300

    feat: Add the School module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import CourseListComponent from './courses/course-list.component';
-import { FreestyleComponent } from './freestyle/freestyle.component';
+import { SchoolModule } from './school/school.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CourseListComponent,
-    FreestyleComponent,
-
-  ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    SchoolModule
+  ],
+  declarations: [
+    AppComponent,
   ],
   providers: [],
   bootstrap: [AppComponent]
