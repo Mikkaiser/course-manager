cb429dfc669d054995858dab5c027665ce99718a
Mikael <mikael.r.simoes@hotmail.com>
2021-05-10 20:11:00 -0300

    feat: Add course.service.ts

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { Course } from './course';
+import { CourseService } from "./course.service";
 
 @Component({
     selector: 'app-course-list',
@@ -7,29 +8,9 @@ import { Course } from './course';
 })
 export default class CourseListComponent implements OnInit{
     courses: Course[] = [];
+    constructor(private courseService: CourseService) { }
 
     ngOnInit() : void {
-        this.courses = [
-            {
-                id: 1,
-                name: 'Mikael Course',
-                imageUrl: '/assets/images/forms.png',
-                price: 47.90,
-                code: 'XPS-5467',
-                duration: 56,
-                rating: 4.5,
-                release: 'December, 2, 2020'
-            },
-            {
-                id: 2,
-                name: 'Ribeiro Course',
-                imageUrl: '/assets/images/http.png',
-                price: 56.99,
-                code: 'XPS-8667',
-                duration: 120,
-                rating: 3,
-                release: 'January, 5, 2021'
-            }
-        ]
+      this.courses = this.courseService.retriveAll();
     }
 }
